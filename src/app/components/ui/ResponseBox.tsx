d781059d4f395fd68aa223db2a7d17bbd2bdef7b
James Nguyen <haimenguyen@gmail.com>
2025-10-19 09:10:17 -0700

    :sparkles: added component for displaying text & json

--- src/app/components/ui/ResponseBox.tsx
+++ src/app/components/ui/ResponseBox.tsx
@@ -1,13 +1,21 @@
 type ResponseBoxProps = {
-  data : string 
+  data : string | object
   title: string
   maxHeight?: string
 }
 
 export function ResponseBox({data, title, maxHeight = "50vh"}: ResponseBoxProps) {
   return (
-    <section aria-label={title} style={{ maxHeight }}>
-      <p>{data}</p>
+    <section 
+      aria-label={title} 
+      style={{ maxHeight }}
+      className="rounded-2xl bg-gray-100 p-4 shadow-sm overflow-auto text-sm"
+    >
+      { typeof data === "string" ? (
+        <p className="text-black-300 text-center">{data}</p>
+      ) : (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      )}
     </section>
   );
 }
