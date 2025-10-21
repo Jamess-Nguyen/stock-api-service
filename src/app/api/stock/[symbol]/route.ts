d3e9734177ee22a3b6a55c9dbcf6388dd791beab
James Nguyen <haimenguyen@gmail.com>
2025-10-20 17:43:59 -0700

    :sparkles: creates object of most recent close date of stock

--- src/app/api/stock/[symbol]/route.ts
+++ src/app/api/stock/[symbol]/route.ts
@@ -5,16 +5,70 @@ type symbolData = {
   "url": string
 }
 
-async function fetchStooqEod(symbol: string){
-  const url = `https://stooq.com/q/d/l/?s=${symbol}&i=d&d1=20250901&d2=20250930`;
+type DateRange = { 
+  startDate: string
+  endDate: string 
+};
+
+type StooqStock = {
+  symbol: string
+  date: string  // "YYYY-MM-DD"
+  open: number
+  high: number
+  low: number
+  close: number
+  volume: number
+};
+
+function formatYYYYMMDD(date: Date): string {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}${month}${day}`;
+}
+
+function getStartEndDates(dateOffset: number): DateRange {
+  const startDate = new Date();
+  startDate.setDate(startDate.getDate()-dateOffset);
+  const endDate = new Date();
+  endDate.setDate(endDate.getDate());
+  return {
+    startDate: formatYYYYMMDD(startDate),
+    endDate: formatYYYYMMDD(endDate)
+  };
+
+}
+
+async function fetchStooqEod(symbol: string) {
+  const dayRange = 2;
+  const { startDate, endDate } = getStartEndDates(dayRange);
+  const url = `https://stooq.com/q/d/l/?s=${symbol}&i=d&d1=${startDate}&d2=${endDate}`;
   const stockResponse = await fetch(url, { cache: 'no-store' });
   const stockCsv = await stockResponse.text();
-  const [header, ...lines] = stockCsv.split(/\r?\n/);
-  console.log("i'm here")
-  console.log(stockResponse)
-  lines.forEach((line) => {
-    console.log(line)
+
+  const [header, ...rows] = stockCsv.split(/\r?\n/);
+  const stockRows: StooqStock[] = rows
+    .filter((cell) => {
+      return cell.length > 0;
+    })
+    .map((line) => {
+      const [date, open, high, low, close, volume] = line.split(",");
+      const rowVal = {
+        symbol: symbol,
+        date: date,
+        open: Number(open),
+        high: Number(high),
+        low: Number(low),
+        close: Number(close),
+        volume: Number(volume),
+      };
+      return rowVal;
+    });
+
+  stockRows.forEach((row) => {
+    console.log(row);
   });
+
 }
 
 export async function GET(request: Request){
